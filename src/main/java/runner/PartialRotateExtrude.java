5a2d2b3dfc154ef336ec8fe0c94dfee864dce1ad
rcpedersen <rcvpedersen@gmail.com>
2016-04-21 19:16:36 -0700

    initial commit of partial rotate extrude

---
+++ src/main/java/runner/PartialRotateExtrude.java
@@ -0,0 +1,32 @@
+package runner;
+
+import java.io.IOException;
+
+import meshmaker.MeshMaker;
+import shape.ImageRotated;
+import shape.Shape;
+
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+
+public class PartialRotateExtrude {
+
+    @Parameter(names = "-inputFile", description = "File to read as an input", required = true)
+    String inputFile;
+    @Parameter(names = "-outputFile", description = "File to write to", required = true)
+    String outputFile;
+    @Parameter(names = "-detail", description = "Amount of detail to compute. (Default 300)")
+    int detail = 300;
+
+    public static void main(String[] args) throws IOException {
+        PartialRotateExtrude rotateExtrude = new PartialRotateExtrude();
+        new JCommander(rotateExtrude, args);
+        rotateExtrude.run();
+    }
+
+    public void run() throws IOException {
+        Shape extrusion = new ImageRotated(inputFile).scaleToSize(detail);
+        MeshMaker maker = new MeshMaker(extrusion, 100);
+        maker.writeMesh(outputFile);
+    }
+}
---
+++ src/main/java/shape/ImageRotated.java
@@ -0,0 +1,55 @@
+package shape;
+
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.vecmath.Point3d;
+
+public class ImageRotated extends Shape {
+    private BufferedImage img;
+
+    public ImageRotated(String path) throws IOException {
+        img = ImageIO.read(new File(path));
+    }
+
+    @Override
+    public boolean inShape(Point3d point) {
+
+        double distFromY = Math.sqrt(point.x * point.x + point.z * point.z);
+        if (distFromY >= img.getWidth() || point.y < 0
+                || point.y >= img.getHeight()) {
+            return false;
+        }
+        int rgb = img.getRGB((int) distFromY, (int) point.y);
+        Color color = new Color(rgb);
+        double greyscale = (color.getRed() + color.getGreen() + color.getBlue()) / 3;
+        // if (greyscale != 0)
+        // System.out.println(greyscale);
+        greyscale = 255 - greyscale;
+        // now black = 255; white = 0
+        if (greyscale == 255) {
+            return true;
+        }
+        if (greyscale == 0) {
+            return false;
+        }
+        double angleDeg = ((Math.atan2(point.z, point.x) * 180 / Math.PI) + 360) % 360;
+        int period = 30;
+        double cycle = angleDeg % period;
+        if (cycle / period < greyscale / 255) {
+            return true;
+        }
+        return false;
+
+    }
+
+    @Override
+    public double[] getBounds() {
+        return new double[] { -img.getWidth(), img.getWidth(), 0,
+                img.getHeight(), -img.getWidth(), img.getWidth() };
+    }
+
+}
